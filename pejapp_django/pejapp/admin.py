e2009fa80294605e7f013d3a59b4b894c1ee9767
zapalke <103122796+zapalke@users.noreply.github.com>
2025-03-20 12:17:29 +0100

    Added filter to bottle search results

--- pejapp_django/pejapp/admin.py
+++ pejapp_django/pejapp/admin.py
@@ -1,25 +1,32 @@
 from django.contrib import admin
 from .models import Post, Profile
 
+
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'bio')
-    search_fields = ('user__username',)
-    list_filter = ('user',)
-    fieldsets = (
-        (None, {
-            'fields': ('user', 'bio')
-        }),
-    )
+    list_display = ("user", "bio")
+    search_fields = ("user__username",)
+    list_filter = ("user",)
+    fieldsets = ((None, {"fields": ("user", "bio")}),)
+
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'date_posted', 'last_modified', 'modified_flag')
-    search_fields = ('title', 'author__username', 'content')
-    list_filter = ('date_posted', 'last_modified', 'modified_flag')
-    readonly_fields = ('date_posted', 'last_modified')
+    list_display = ("title", "author", "date_posted", "last_modified", "modified_flag")
+    search_fields = ("title", "author__username", "content")
+    list_filter = ("date_posted", "last_modified", "modified_flag")
+    readonly_fields = ("date_posted", "last_modified")
     fieldsets = (
-        (None, {
-            'fields': ('title', 'author', 'content', 'modified_flag', 'original_content')
-        }),
+        (
+            None,
+            {
+                "fields": (
+                    "title",
+                    "author",
+                    "content",
+                    "modified_flag",
+                    "original_content",
+                )
+            },
+        ),
     )
